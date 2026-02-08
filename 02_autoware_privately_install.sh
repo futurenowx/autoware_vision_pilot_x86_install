685fa34369adb6e684575917002b06df12094c53
delivery-me autonomous robotics <46852114+futurenowx@users.noreply.github.com>
2026-02-08 15:45:41 +0100

    Add files via upload

---
+++ 02_autoware_privately_install.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+set -e  # stop on error
+
+# Go to home directory
+
+cd "$HOME"
+
+# Check if folder exists
+
+if [ -d "autoware.privately-owned-vehicles" ]; then
+    mv autoware.privately-owned-vehicles autoware.privately-owned-vehicles_old
+    echo "✅ Folder renamed to autoware.privately-owned-vehicles_old"
+else
+    echo "ℹ️ Folder autoware.privately-owned-vehicles not found, skipping rename"
+fi
+
+cd "$HOME"
+
+# Clone Autoware repository
+
+git clone https://github.com/autowarefoundation/autoware.privately-owned-vehicles.git
+
+# Rename Models folder
+
+mv autoware.privately-owned-vehicles/Models/visualizations \
+   autoware.privately-owned-vehicles/Models/visualizations_old
+
+# Go to $HOME/autoware_privately_x86_install folder
+
+cd "$HOME/autoware_privately_x86_install" || { echo "❌ Cannot enter autoware_privately_x86_install folder"; exit 1; }
+
+# copy folder autoware_projetcs to $HOME
+
+SOURCE="autoware_projetcs"
+DEST="$HOME"
+if [ -d "$SOURCE" ]; then
+    cp -r "$SOURCE" "$DEST"
+    echo "✅ autoware_projetcs folder copied successfully"
+else
+    echo "❌ Error: autoware_projetcs folder does not exist"
+    exit 1
+fi
+
+# copy folder visualization to autoware.privately-owned-vehicles/Models
+
+SOURCE="visualization"
+DEST="$HOME/autoware.privately-owned-vehicles/Models"
+if [ -d "$SOURCE" ]; then
+    cp -r "$SOURCE" "$DEST"
+    echo "✅ visualization folder copied successfully"
+else
+    echo "❌ Error: visualization folder does not exist"
+    exit 1
+fi
+
+echo "✅ Step1 install completed successfully, now download the weights and run 03_autoware_weights_install.sh."
+echo "Please check the install.txt!"
